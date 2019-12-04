8701a1d3513565f352415a56cfe68916f6d4860c
Cameron <cameron@carmichaelalonso.co.uk>
2019-12-04 16:49:31 +0000

    Add .NET Core support

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace InfiniteFlight.ApiClient
+namespace InfiniteFlight.Api
 {
     /// <summary>
     /// Interaction logic for App.xaml
